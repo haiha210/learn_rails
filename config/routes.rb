c3609db24573aab7d4aabc2becd2b70147092fd1
haiha210 <haiha210.gm@gmail.com>
2022-04-22 11:10:02 +0700

    rswag_api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    resources :posts, only: :index
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: :index
+    end
   end
 end
